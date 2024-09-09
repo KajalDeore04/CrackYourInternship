62e56554ad5db90f743eca759c3fe439e14141c7
Kajal Deore <137670233+KajalDeore04@users.noreply.github.com>
2024-09-09 22:50:38 +0530

    validPrentheses.py

---
+++ validPrentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in bracket_map:
+                if stack and stack.pop() == bracket_map[char]:
+                    continue
+                else:
+                    return False
+            else:
+                stack.append(char)
+                
+        return not stack
